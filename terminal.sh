45a98a4233be951576144d9ab7f50f2f63d83646
Бондаренко Дмитрий Антонович <dmetrobond@gmail.com>
2022-12-09 04:51:02 -0500

    1.11 -  вывести файлы по типу

---
+++ terminal.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Каталоги:"
+ls . -a -l |grep ^d
+echo "Обычные файлы:"
+ls . -a -l | grep ^-
+echo "Символьные ссылки"
+ls . -a -l | grep ^l
+echo "Символьные устройства"
+ls . -a -l | grep ^c
+echo "Блочные устройства"
+ls . -a -l | grep ^b
+
